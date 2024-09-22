c64fc76b47276b3a4f94b71da8bca7c8c63c21d2
carolineruella <carolineruella@gmail.com>
2024-09-22 11:50:48 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+function pesquisar() {
+
+
+}
+
+// console.log(dados);
+
+let section = document.getElementById("resultados-pesquisa")
+console.log(section)
+
+for (let dado of dados) {
+    section.innerHTML = ''
+
+
+}
+
